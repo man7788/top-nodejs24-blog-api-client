3f29c54c587a77fc44e87a72cf3bba41eb2d114a
man7788 <jerman1841@gmail.com>
2025-10-15 12:15:18 +0800

    add body component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import styles from './App.module.css';
 import Header from './components/header/Header';
+import Body from './components/body/Body';
 
 function App() {
   return (
     <div className={styles.App}>
       <Header />
+      <Body />
     </div>
   );
 }
