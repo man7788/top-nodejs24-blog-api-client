f8fe365b479e5ec4c62bf4840cb2443a60f98f51
man7788 <jerman1841@gmail.com>
2025-10-15 14:20:14 +0800

    add children index

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,11 +1,18 @@
 import App from '../App';
 import ErrorPage from '../components/error/ErrorPage';
+import Home from '../components/home/Home';
 
 const routes = [
   {
     path: '/',
     element: <App />,
     errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+    ],
   },
 ];
 
