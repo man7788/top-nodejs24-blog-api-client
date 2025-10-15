9ed022c989a2ce6b13e4ba455e65facaa4724baa
man7788 <jerman1841@gmail.com>
2025-10-15 14:28:05 +0800

    add post component

--- src/components/home/posts/PostList.jsx
+++ src/components/home/posts/PostList.jsx
@@ -1,9 +1,11 @@
 import styles from './PostList.module.css';
+import Post from './Post';
 
 const PostList = () => {
   return (
     <div className={styles.PostList}>
       <h1>Post List</h1>
+      <Post />
     </div>
   );
 };
