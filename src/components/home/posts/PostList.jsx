2417995a5ab2315bc70463d1a0b6fc0d90958019
man7788 <jerman1841@gmail.com>
2025-10-15 14:19:44 +0800

    create post list component

---
+++ src/components/home/posts/PostList.jsx
@@ -0,0 +1,11 @@
+import styles from './PostList.module.css';
+
+const PostList = () => {
+  return (
+    <div className={styles.PostList}>
+      <h1>Post List</h1>
+    </div>
+  );
+};
+
+export default PostList;
