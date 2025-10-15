cf53e29e767f56294ac8425cefc6ccee4a345fae
man7788 <jerman1841@gmail.com>
2025-10-15 11:23:13 +0800

    update render to use react-router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
+import './index.module.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.module.css';
-import App from './App.jsx';
+import { createBrowserRouter, RouterProvider } from 'react-router';
+import routes from './routes/routes';
+
+const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 );
