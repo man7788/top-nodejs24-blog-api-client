59f7c67a0216119085f1a6bbb3bf5531e757b6e8
man7788 <jerman1841@gmail.com>
2025-10-16 03:49:58 +0800

    update home list render

--- src/components/home/list/HomeList.jsx
+++ src/components/home/list/HomeList.jsx
@@ -1,11 +1,32 @@
 import styles from './HomeList.module.css';
-import PostPreview from './list-item/PostPreview';
+import PostPreview from './item/PostPreview';
 
 const HomeList = () => {
+  const posts = [
+    {
+      id: 1,
+      title: 'Title for the first post',
+      content: 'Content for the first post.',
+      createdAt: '2025-10-15T19:20:06.240Z',
+    },
+    {
+      id: 2,
+      title: 'Title for the second post',
+      content: 'Content for the second post.',
+      createdAt: '2025-10-15T19:20:06.240Z',
+    },
+  ];
+
   return (
-    <div className={styles.PostList}>
-      <h1>Home List</h1>
-      <PostPreview />
+    <div className={styles.HomeList}>
+      {posts.map((post) => (
+        <PostPreview
+          key={post.id}
+          id={post.id}
+          title={post.title}
+          created={post.createdAt}
+        />
+      ))}
     </div>
   );
 };
