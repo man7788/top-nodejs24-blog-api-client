cac4075859c5201fbd60e123e4ad4e845294f0f8
man7788 <jerman1841@gmail.com>
2025-10-16 03:02:50 +0800

    add comment form component

--- src/components/post/comment/Comment.jsx
+++ src/components/post/comment/Comment.jsx
@@ -1,9 +1,11 @@
 import styles from './Comment.module.css';
+import CommentForm from './form/CommentForm';
 
 const Comment = ({ id }) => {
   return (
     <div className={styles.Comment}>
       <h2>Comment ID: {id}</h2>
+      <CommentForm />
     </div>
   );
 };
