cdbc46e58ea2c52d5da33602639fecf3ddd2052b
man7788 <jerman1841@gmail.com>
2025-10-16 02:33:16 +0800

    add comment component

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,12 +1,18 @@
 import styles from './Post.module.css';
 import { useParams } from 'react-router';
+import Comment from './comment/Comment';
 
 const Post = () => {
   const { id } = useParams();
 
+  const comments = [{ id: 1 }];
+
   return (
     <div className={styles.Post}>
       <h1>Post ID: {id}</h1>
+      {comments.map((comment) => (
+        <Comment key={comment.id} id={comment.id} />
+      ))}
     </div>
   );
 };
