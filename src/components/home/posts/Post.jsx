8d6978842ff90b5dd1fb75a8960209fd72043221
man7788 <jerman1841@gmail.com>
2025-10-15 14:27:46 +0800

    create post component

---
+++ src/components/home/posts/Post.jsx
@@ -0,0 +1,11 @@
+import styles from './Post.module.css';
+
+const Post = () => {
+  return (
+    <div className={styles.Post}>
+      <h2>Post Title</h2>
+    </div>
+  );
+};
+
+export default Post;
