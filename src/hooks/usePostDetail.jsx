6ccf6621c55ccccc380398839137237354bd5408
man7788 <jerman1841@gmail.com>
2025-10-16 11:49:11 +0800

    create post detail hook

---
+++ src/hooks/usePostDetail.jsx
@@ -0,0 +1,46 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router';
+
+const usePostDetail = () => {
+  const { id } = useParams();
+
+  const [post, setPost] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `${import.meta.env.VITE_API_URL}/posts/${id}`,
+          {
+            mode: 'cors',
+          },
+        );
+
+        if (response.status === 404) {
+          throw new Error('Post Not Found');
+        }
+
+        if (response.status >= 400) {
+          throw new Error('Server Error');
+        }
+
+        const result = await response.json();
+        const data = result.data.post;
+
+        setPost(data);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [id]);
+
+  return { post, error, loading };
+};
+
+export default usePostDetail;
