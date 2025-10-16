93a3218762a3313c3393d63c1ea497d56ee2666b
man7788 <jerman1841@gmail.com>
2025-10-16 09:26:39 +0800

    create post list hook

---
+++ src/hooks/usePostList.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react';
+
+const usePostList = () => {
+  const [posts, setPosts] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${import.meta.env.VITE_API_URL}/posts`, {
+          mode: 'cors',
+        });
+        const result = await response.json();
+        const data = result.data.posts;
+
+        setPosts(data);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return { posts, error, loading };
+};
+
+export default usePostList;
