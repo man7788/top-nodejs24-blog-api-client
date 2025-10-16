d2765e7b34455522b8cb655ab8b52b3d357a50a1
man7788 <jerman1841@gmail.com>
2025-10-16 09:28:32 +0800

    add render post list hook

--- src/components/home/list/HomeList.jsx
+++ src/components/home/list/HomeList.jsx
@@ -1,21 +1,13 @@
 import styles from './HomeList.module.css';
 import PostPreview from './item/PostPreview';
+import usePostList from '../../../hooks/usePostList';
 
 const HomeList = () => {
-  const posts = [
-    {
-      id: 1,
-      title: 'Title for the first post',
-      content: 'Content for the first post.',
-      createdAt: '2025-10-15T19:20:06.240Z',
-    },
-    {
-      id: 2,
-      title: 'Title for the second post',
-      content: 'Content for the second post.',
-      createdAt: '2025-10-15T19:20:06.240Z',
-    },
-  ];
+  const { posts, loading, error } = usePostList();
+
+  if (loading) return <p>Loading...</p>;
+
+  if (error) return <p>A network error was encountered</p>;
 
   return (
     <div className={styles.HomeList}>
