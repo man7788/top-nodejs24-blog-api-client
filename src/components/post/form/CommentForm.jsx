299ee9da1a62a0b4851345afe622118d6c17f6aa
man7788 <jerman1841@gmail.com>
2025-10-16 08:44:05 +0800

    update comment form render

--- src/components/post/form/CommentForm.jsx
+++ src/components/post/form/CommentForm.jsx
@@ -1,21 +1,45 @@
 import styles from './CommentForm.module.css';
+import { useState } from 'react';
 
 const CommentForm = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [content, setContent] = useState('');
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+  };
+
   return (
     <div className={styles.CommentForm}>
       <h4>Leave a comment</h4>
-      <form method="post">
+      <form method="post" onSubmit={submitForm}>
         <label htmlFor="name">Name:</label>
-        <input type="text" id="name" name="name" />
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+        />
         <br></br>
         <label htmlFor="email">Email:</label>
-        <input type="email" id="email" name="email" />
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
         <br></br>
         <textarea
           name="content"
-          defaultValue="Your comment here"
           rows="5"
           cols="30"
+          placeholder="Your comment here"
+          value={content}
+          onChange={(event) => setContent(event.target.value)}
         ></textarea>
         <br></br>
         <input type="submit" value="Submit" />
