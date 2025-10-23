8bb9cd5424c43f2225fd7903a968e27612ced478
man7788 <jerman1841@gmail.com>
2025-10-24 02:36:22 +0800

    update post preview render

--- src/components/home/list/item/PostPreview.jsx
+++ src/components/home/list/item/PostPreview.jsx
@@ -1,13 +1,16 @@
 import styles from './PostPreview.module.css';
 import { Link } from 'react-router';
+import dateFormatter from '../../../../utils/dateFormatter';
 
 const Post = ({ id, title, created }) => {
+  const formattedDate = dateFormatter(created);
+
   return (
     <div className={styles.PostPreview}>
-      <Link to={`/posts/${id}`}>
-        <h2>{title}</h2>
+      <Link className={styles.link} to={`/posts/${id}`}>
+        <h2 className={styles.title}>{title}</h2>
       </Link>
-      {created}
+      <div className={styles.date}>{formattedDate}</div>
     </div>
   );
 };
