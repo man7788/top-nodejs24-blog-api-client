715838775c1ca6fda21cd5df9529e2c035794d15
man7788 <jerman1841@gmail.com>
2025-10-24 02:10:04 +0800

    add footer to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import styles from './App.module.css';
 import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
 import { Outlet } from 'react-router';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className={styles.App}>
       <Header />
       <Outlet />
+      <Footer />
     </div>
   );
 }
