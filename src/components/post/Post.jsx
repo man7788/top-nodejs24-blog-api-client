fc89446f8b0293b251cd77f3e24b7dcf43df3d8b
man7788 <jerman1841@gmail.com>
2025-10-24 03:24:40 +0800

    update post render

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,10 +1,12 @@
 import styles from './Post.module.css';
 import usePostDetail from '../../hooks/usePostDetail';
+import dateFormatter from '../../utils/dateFormatter';
 import Comment from './comment/Comment';
 import CommentForm from './form/CommentForm';
 
 const Post = () => {
   const { post, loading, error, update, setUpdate } = usePostDetail();
+  const formattedDate = dateFormatter(post?.createdAt);
 
   if (loading) return <p>Loading...</p>;
 
@@ -18,9 +20,12 @@ const Post = () => {
 
   return (
     <div className={styles.Post}>
-      <h1>{post.title}</h1>
-      {post.content}
-      <h3>Comments:</h3>
+      <h2 className={styles.title}>
+        {post.title}
+        <div className={styles.createdAt}>{formattedDate}</div>
+      </h2>
+      <p className={styles.content}>{post.content}</p>
+      <h4 className={styles.comments}>Comments</h4>
       {post.comments.map((comment) => (
         <Comment
           key={comment.id}
