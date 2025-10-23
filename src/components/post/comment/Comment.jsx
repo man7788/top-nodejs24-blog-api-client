1c2ae518467b4b110fb9fac263221952c35174cb
man7788 <jerman1841@gmail.com>
2025-10-24 03:25:39 +0800

    update comment render

--- src/components/post/comment/Comment.jsx
+++ src/components/post/comment/Comment.jsx
@@ -1,11 +1,15 @@
 import styles from './Comment.module.css';
+import dateFormatter from '../../../utils/dateFormatter';
 
 const Comment = ({ name, content, created }) => {
+  const formattedDate = dateFormatter(created);
+
   return (
     <div className={styles.Comment}>
-      <h4>{name}</h4>
-      <span>{content}</span>
-      {created}
+      <h5 className={styles.author}>
+        {name} • <span className={styles.createdAt}>{formattedDate}</span>
+      </h5>
+      <p className={styles.content}>{content}</p>
     </div>
   );
 };
