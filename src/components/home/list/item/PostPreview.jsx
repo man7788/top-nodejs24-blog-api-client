6b38ae9eb4273b077aee009e71fbffefbc907474
man7788 <jerman1841@gmail.com>
2025-10-29 04:33:02 +0800

    update post preview render

--- src/components/home/list/item/PostPreview.jsx
+++ src/components/home/list/item/PostPreview.jsx
@@ -6,12 +6,12 @@ const Post = ({ id, title, created }) => {
   const formattedDate = dateFormatter(created);
 
   return (
-    <div className={styles.PostPreview}>
+    <li className={styles.PostPreview}>
       <Link className={styles.link} to={`/posts/${id}`}>
         <h2 className={styles.title}>{title}</h2>
       </Link>
       <div className={styles.date}>{formattedDate}</div>
-    </div>
+    </li>
   );
 };
 
