95bb3e3dce629ae3c49778cc75a9a88ac0fed0fd
man7788 <jerman1841@gmail.com>
2025-10-29 04:32:45 +0800

    update routes

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -2,6 +2,8 @@ import App from '../App';
 import ErrorPage from '../components/error/ErrorPage';
 import Home from '../components/home/Home';
 import Post from '../components/post/Post';
+import About from '../components/about/About';
+import Contact from '../components/contact/Contact';
 
 const routes = [
   {
@@ -16,6 +18,14 @@ const routes = [
       {
         path: '/posts/:id',
         element: <Post />,
+      },
+      {
+        path: '/about',
+        element: <About />,
+      },
+      {
+        path: '/contact',
+        element: <Contact />,
       },
     ],
   },
